940f351f042670a9192c745a939381d686a65565
Aperturefly <161852793+Aperturefly@users.noreply.github.com>
2025-05-23 10:26:25 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,13 @@ menu_option = int(input("Press 1 to play, press 2 to explain the rules "))
 if menu_option == 1:
     card_two = random.randint(1, 13)
     card_one = random.randint(1, 13)
-    
+    dealer_one = random.randint(1, 13)
+    if dealer_one == 11:
+        dealer_one = 10
+    elif dealer_one == 12:
+        dealer_one = 10
+    elif dealer_one == 13:
+        dealer_one = 10
     if card_one == 11:
         card_one = 10
     elif card_one == 12:
@@ -18,6 +24,5 @@ if menu_option == 1:
     elif card_two == 13:
         card_two = 10
     print(("Ok, your cards are a "),  card_one,  ("and a"),  card_two,)
-#pygame
-#Define values for cards
+    print(("My cards are a "), dealer_one, (" and a ???")
 #code rng
